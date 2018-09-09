5fef7a2b8ba2a7fb72980924dbdc8ec967845833
viniciusrvk <viniciusrvk@U-PC>
2018-09-09 16:41:36 -0300

    classe do login

---
+++ DGEOsig/src/view/loginClass.java
@@ -0,0 +1,23 @@
+package view;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class loginClass {
+
+    @FXML
+    private TextField login;
+
+    @FXML
+    private Label logo;
+
+    @FXML
+    private Button start;
+
+    @FXML
+    void start(ActionEvent event) {
+
+    }
+
+}
