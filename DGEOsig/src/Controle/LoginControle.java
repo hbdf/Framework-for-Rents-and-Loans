69a1c095cb93f4467767f6ebccd148dca4b1c777
hbdf <heliobduartefilho@gmail.com>
2018-09-10 21:21:35 -0300

    Checa se login está correto. Problema com abrir tela Principal.
    Tentei criar a classe TelaPrincipalClass para abrir a tela principal(Ví
    que tinha que está tudo no mesmo pacote), mas mesmo assim ainda não está
    funcionando.

--- DGEOsig/src/Controle/LoginControle.java
+++ DGEOsig/src/Controle/LoginControle.java
@@ -1,21 +1,30 @@
 package Controle;
 
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import objeto.modelo.LoginDAO;
 import objeto.modelo.LoginDAOSemBanco;
+import view.TelaPrincipalClass;
 
 public class LoginControle 
 {
-	public void PodeEntrar (String usuario, String senha)
+	public void PodeEntrar (String usuario, String senha) throws IOException
 	{
 		LoginDAO veri = new LoginDAOSemBanco();
 		if (veri.temUsuario(usuario) && veri.getSenha(usuario).equals(senha))
 		{
-			
+			TelaPrincipalClass MainScreen = new TelaPrincipalClass();
+			MainScreen.open();
 		}
 		else 
 		{
 			//TODO Show Screen with Wrong User/Password
-			
+			System.out.println("Deu Ruim");
 		}
 	}
 }
--- DGEOsig/src/view/loginClass.java
+++ DGEOsig/src/view/loginClass.java
@@ -1,16 +1,25 @@
 package view;
+import java.io.IOException;
+
+import Controle.LoginControle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class loginClass 
 {
 
+	LoginControle lg = new LoginControle();
     @FXML
     private TextField login;
 
+    @FXML
+    private PasswordField senha;
+    
     @FXML
     private Label logo;
 
@@ -20,7 +29,12 @@ public class loginClass
     @FXML
     void start(ActionEvent event) 
     {
-    	System.out.println("teste");
+    	try {
+			lg.PodeEntrar(login.getText(), senha.getText());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
 }
