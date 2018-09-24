db3e9f41d5dbf29d9b1ff9d528329b7f7836af0f
viniciusrvk <viniciusrvk@SRI>
2018-09-24 13:53:53 -0300

    Adicionado Label pra mostrar o servidor logado, mas ainda nao funcional

--- DGEOsig/src/view/TelaPrincipalClass.java
+++ DGEOsig/src/view/TelaPrincipalClass.java
@@ -2,34 +2,41 @@
 package view;
 
 import java.io.IOException;
-import java.net.URL;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class TelaPrincipalClass {
 	
+	String usuario;
+	
+	@FXML
+    private AnchorPane TelaPrincipal;
+    @FXML
+    private MenuItem CadastrarAluno;
+    @FXML
+    private Label lblServidor;
+
+
 	public void open() throws IOException 
-	{
+	{	
+
 		Stage primaryStage = new Stage();
 		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/TelaPrincipal.fxml"));
 		Scene scene = new Scene (root);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		System.out.println("OK!");
 	}
-	@FXML
-    private AnchorPane TelaPrincipal;
 
-    @FXML
-    private MenuItem CadastrarAluno;
-	
 	public void CadastrarAluno(ActionEvent event) {
 		TelaCadastrarAluno cad = new TelaCadastrarAluno();
 		try {
