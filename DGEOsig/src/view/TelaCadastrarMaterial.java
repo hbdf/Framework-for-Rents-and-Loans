5a4067232147a80187691aef8723ee8c6db64f9a
viniciusrvk <viniciusrvk@SRI>
2018-09-24 12:52:04 -0300

    Criados mas nao implementados

--- DGEOsig/src/view/TelaCadastrarMaterial.java
+++ DGEOsig/src/view/TelaCadastrarMaterial.java
@@ -6,7 +6,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class CadastrarMaterialClass {
+public class TelaCadastrarMaterial {
 
     @FXML
     private AnchorPane TelaPrincipal;
