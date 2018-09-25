ef0740a0c0e2ba49896beb872a4c186a5cd27967
viniciusrk <viniciusrvk@U-PC>
2018-09-25 01:46:15 -0300

    Telas Ok; falta logica pro banco de dados

---
+++ DGEOsig/src/Controle/CadastrarMaterialControle.java
@@ -0,0 +1,19 @@
+package Controle;
+
+import objeto.modelo.Material;
+
+public class CadastrarMaterialControle {
+
+	public void cadastrar(String nome, String numeroTombamento, String serie, String descricao) {
+		
+		System.out.println("material cadastrado!");
+		
+	}
+	public Material busca(String chave) {
+		return new Material("1111", "Martelo");// TESTE
+	}
+	public void remover(String chave) {
+		System.out.println("Removeu Material");
+	}
+
+}
--- DGEOsig/src/view/TelaCadastrarMaterial.java
+++ DGEOsig/src/view/TelaCadastrarMaterial.java
@@ -1,28 +1,67 @@
 package view;
 
+import Controle.CadastrarMaterialControle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class TelaCadastrarMaterial {
-
     @FXML
     private AnchorPane TelaPrincipal;
-
     @FXML
-    private TextField numeroTombamento;
-
+    private TextField tfTombamento;
     @FXML
-    private TextField nomeMaterial;
-
+    private TextField tfNome;
     @FXML
-    private TextArea descricao;
+    private TextArea taDescricao;
+    @FXML
+    private TextField tfSerie;
+    @FXML
+    private Button btnCadastrar;
+    @FXML
+    private Button btnCancelar;
 
     @FXML
     void cadastrarMaterial(ActionEvent event) {
+    	String numeroTombamento = tfTombamento.getText();
+    	String nome = tfNome.getText();
+    	String serie = tfSerie.getText();
+    	String descricao = taDescricao.getText();
     	
+    	new CadastrarMaterialControle().cadastrar(nome, numeroTombamento, serie, descricao);
+    	
+    	//close
+    	this.close();
     }
+
+    @FXML
+    void cancelar(ActionEvent event) {
+    	this.close();
+    }
+
+	public void open() {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/view/TelaCadastrarMaterial.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.setTitle("Cadastrar Material");
+			stage.show();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	private void close() {
+		Stage stage = (Stage) btnCancelar.getScene().getWindow();
+    	stage.close();
+	}
 
 }
