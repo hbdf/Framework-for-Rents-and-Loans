c965a1a563ff850cca3ed8356ab7517c25c2554f
helio <heliobduartefilho@gmail.com>
2018-10-09 08:11:30 -0300

    .

--- DGEOsig/src/controle/TelaCadastrarMaterial.java
+++ DGEOsig/src/controle/TelaCadastrarMaterial.java
@@ -47,7 +47,7 @@ public class TelaCadastrarMaterial implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
-		initComboBoxTipos();
+		initComboBoxTipos(); 
 	}
 	
 	@FXML
