bb0667a156ccca9876c1e40516c1d35bc03ffb0a
helio <heliobduartefilho@gmail.com>
2018-10-17 16:39:32 -0300

    Add comentarios

--- DGEOsig/src/servico/UsuarioControle.java
+++ DGEOsig/src/servico/UsuarioControle.java
@@ -5,14 +5,14 @@ import java.util.List;
 import dao.UsuarioDAO;
 import modelo.Usuario;
 
-
+/*
+ *  Essa Classe é responsável por todo o Controle sob as operações de Usuário
+ */
 
 public final class UsuarioControle {
-	
 	/*
-	 * TODO RELAIZAR O CONTROLE DO USUARIO (CRIAR UM MÉTODO VALIDAÇÃO)
+	 * Singlenton Pattern
 	 */
-	
 	private static UsuarioControle instance;
 	public static UsuarioControle getInstance() {
 		if(instance == null) {
@@ -21,11 +21,20 @@ public final class UsuarioControle {
 		return instance;
 	}
 	private UsuarioControle () {}
+	
+	/*
+	 * Teste se algum campo está vazio
+	 */
 	void CamposVazios(Usuario usuario) throws Exception{
 		if(usuario.get_cpf().isEmpty() || usuario.get_email().isEmpty() || usuario.get_matricula().isEmpty() || usuario.get_nome().isEmpty()) {
 			throw new Exception("Todos os campos devem ser preenchidos");
 		}
 	}
+	
+	/*
+	 * Cadastra um usuario.
+	 * Método completo com lançamento de exceção.
+	 */
 	public void cadastrar(Usuario usuario) throws Exception {
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		CamposVazios(usuario);
@@ -34,10 +43,12 @@ public final class UsuarioControle {
 		}
 		usuarioDAO.cadastrar(usuario);
 	}
+	
 	public List<Usuario> buscar_matricula(String matricula) {
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		return usuarioDAO.buscar_matricula(matricula);
 	}
+	
 	public List<Usuario> buscar_CPF(String CPF) {
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		return usuarioDAO.buscar_CPF(CPF);
